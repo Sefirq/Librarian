afe9b4c5da84dbda7e49200a7d3fb865a40bd8de
Adam Pioterek <adam.pioterek@protonmail.ch>
2016-11-29 17:31:07 +0100

    JavaFX Hello World

---
+++ desktop/src/ml/adamsprogs/librarian/Controller.java
@@ -0,0 +1,12 @@
+package ml.adamsprogs.librarian;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Controller {
+
+    @FXML
+    public void onButtonPressed(ActionEvent actionEvent) {
+        System.out.println("The button was pressed!");
+    }
+}
